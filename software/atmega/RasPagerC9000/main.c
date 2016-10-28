ab391083aa54b61f0ef5ca51494e4bb08cafdd2c
Ralf Wilke <wilke@ihf.rwth-aachen.de>
2016-10-28 10:40:22 +0200

    Added first starting point code for ATmega and Atmel Studio 7.0 project files

---
+++ software/atmega/RasPagerC9000/main.c
@@ -0,0 +1,29 @@
+/*
+ * RasPagerC9000.c
+ *
+ * Created: 27.10.2016 16:22:03
+ * Author : wilke
+ */ 
+
+#include "config.h"
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+
+
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
