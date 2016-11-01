0dfb916341f882cc1fa72d8cc5a8e06d6a1c8b35
Thomas Gatzweiler <mail@thomasgatzweiler.com>
2016-11-01 12:03:09 +0000

    Improve names and descriptions in config.h

--- software/atmega/config.h
+++ software/atmega/config.h
@@ -23,31 +23,31 @@
 #define LED_GREEN_PIN       PINB
 #define LED_GREEN_BIT       PINB2
 
-// PTT_C9000 -> This pin drives the PTT line of C9000, High means C9000 is set to Ground = Low
-#define PTT_C9000_DDR       DDRD
-#define PTT_C9000_PORT      PORTD
-#define PTT_C9000_PIN       PIND
-#define PTT_C9000_BIT       PIND5
+// C9000_PTT -> This pin drives the PTT line of C9000, High means C9000 is set to Ground = Low
+#define C9000_PTT_DDR       DDRD
+#define C9000_PTT_PORT      PORTD
+#define C9000_PTT_PIN       PIND
+#define C9000_PTT_BIT       PIND5
 
-// MDL_C9000 -> This pin drives the MDL line of C9000, High means C9000 MDL is set to Ground = Low
-#define MDL_C9000_DDR       DDRD
-#define MDL_C9000_PORT      PORTD
-#define MDL_C9000_PIN       PIND
-#define MDL_C9000_BIT       PIND6
+// C9000_MDL -> This pin drives the MDL line of C9000, High means C9000 MDL is set to Ground = Low
+#define C9000_MDL_DDR       DDRD
+#define C9000_MDL_PORT      PORTD
+#define C9000_MDL_PIN       PIND
+#define C9000_MDL_BIT       PIND6
 
-// MDE_C9000 -> This pin drives the MDE line of C9000, High means C9000 MDE is set to Ground = Low
-#define MDE_C9000_DDR       DDRD
-#define MDE_C9000_PORT      PORTD
-#define MDE_C9000_PIN       PIND
-#define MDE_C9000_BIT       PIND7
+// C9000_MDE -> This pin drives the MDE line of C9000, High means C9000 MDE is set to Ground = Low
+#define C9000_MDE_DDR       DDRD
+#define C9000_MDE_PORT      PORTD
+#define C9000_MDE_PIN       PIND
+#define C9000_MDE_BIT       PIND7
 
-// RASPI_SENDATA -> This pin drives the MDE line of C9000, High means C9000 MDL is set to Ground = Low
-#define RASPI_SENDDATA_DDR   DDRC
-#define RASPI_SENDDATA_PORT  PORTC
-#define RASPI_SENDDATA_PIN   PINC
-#define RASPI_SENDDATA_BIT   PINC0
+// RASPI_SENDATA -> This pin signals the Raspberry Pi that data can be received
+#define RASPI_SENDDATA_DDR  DDRC
+#define RASPI_SENDDATA_PORT PORTC
+#define RASPI_SENDDATA_PIN  PINC
+#define RASPI_SENDDATA_BIT  PINC0
 
-// RASPI_PTT -> This pin drives the MDE line of C9000, High means C9000 MDL is set to Ground = Low
+// RASPI_PTT -> This pin goes to High if the Raspberry Pi wants to send data
 #define RASPI_PTT_DDR       DDRC
 #define RASPI_PTT_PORT      PORTC
 #define RASPI_PTT_PIN       PINC
